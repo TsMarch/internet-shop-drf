1f8cc30be8d7cfe2bc5665a51760ca68fa075e3d
TsMarch <dementedcactus@mail.ru>
2024-08-05 19:57:17 +0500

    added "cart" model

--- internet_shop/shop/admin.py
+++ internet_shop/shop/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import ProductCategory, Product
+from .models import ProductCategory, Product, Cart
 
-admin.site.register([Product, ProductCategory])
+admin.site.register([Product, ProductCategory, Cart])
--- internet_shop/shop/models.py
+++ internet_shop/shop/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -6,7 +7,7 @@ class ProductCategory(models.Model):
     name = models.CharField("Название категории", max_length=100)
 
     class Meta:
-        verbose_name_plural = "Product categories"
+        verbose_name_plural = "Категории товаров"
 
     def __str__(self):
         return f"{self.name}"
@@ -20,18 +21,35 @@ class Product(models.Model):
     discount = models.PositiveIntegerField("Процент скидки")
     price = models.DecimalField("Цена со скидкой", decimal_places=2, max_digits=10)
     available = models.BooleanField("Доступность товара", default=True)
-    available_quantity = models.PositiveIntegerField("Остаток товара на складе", default=0)
+    available_quantity = models.PositiveIntegerField(
+        "Остаток товара на складе", default=0
+    )
+
+    class Meta:
+        verbose_name_plural = "Товары"
 
     def __str__(self):
         return f"Название товара: {self.name}; Цена со скидкой: {self.price}; Скидка: {self.discount}%"
 
     @property
     def price(self):
-        return self.old_price - self.old_price * self.discount/100
+        return self.old_price - self.old_price * self.discount / 100
 
     def clean(self):
         if not self.available and self.available_quantity > 0:
-            raise ValidationError("Невозможно наличие товара на складе если он недоступен")
+            raise ValidationError(
+                "Невозможно наличие товара на складе если он недоступен"
+            )
         super().save()
 
 
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Корзина"
+
+    def __str__(self):
+        return f"{self.product.name}"
