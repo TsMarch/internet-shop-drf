69fac095547e1e94b8fdcbdaa943ea25fb8c3e8e
dementedcactus@mail.ru <dementedcactus@mail.ru>
2025-04-17 02:19:39 +0500

    feat (send email task): task template to send email

--- internet_shop/shop/tasks.py
+++ internet_shop/shop/tasks.py
@@ -1,8 +1,8 @@
 from celery import shared_task
+from django.core.mail import EmailMessage
 
 
 @shared_task
-def long_task():
-    print(">>> Starting long task...")
-    print(">>> Long task completed.")
-    return "Finished"
+def send_email(to_email, subject, message):
+    email = EmailMessage(subject, message, "pass", [to_email])
+    email.send(fail_silently=False)
