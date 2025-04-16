8c4477b7a510a654491b8c7ac006c29aba791878
dementedcactus@mail.ru <dementedcactus@mail.ru>
2025-04-17 02:19:00 +0500

    feat (send email signals): signals to send email after order and registration

--- internet_shop/shop/signals.py
+++ internet_shop/shop/signals.py
@@ -1,11 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Product
-from .tasks import long_task
+from .models import Order, Product
 
 
 @receiver(post_save, sender=Product)
 def send_email_on_register(sender, instance, created, **kwargs):
-    print("got signal")
-    long_task.delay()
+    if created:
+        pass
+
+
+@receiver(post_save, sender=Order)
+def send_email_after_order(sender, instance, created, **kwargs):
+    if created:
+        pass
