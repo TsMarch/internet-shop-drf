a1e1755da931f0ac2041668231f95bd705d231a1
TsMarch <dementedcactus@mail.ru>
2025-04-18 21:32:37 +0500

    feat(send email on register signal): added userbalance creation after user registration

--- internet_shop/shop/signals.py
+++ internet_shop/shop/signals.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
+
 from django.db.models.signals import post_save
 from django.dispatch import Signal, receiver
 
-from .models import User
+from .models import User, UserBalance
 from .tasks import send_email
 
 order_fully_created = Signal()
@@ -12,14 +14,15 @@ def send_email_on_register(sender, instance, created, **kwargs):
     if created:
         subject = f"Привет, {instance.username}"
         message = "Привет и добро пожаловать!"
-        recipient = instance.email
+        UserBalance.objects.create(user=instance, balance=Decimal(0))
+        recipient = [instance.email]
         send_email.delay(subject=subject, message=message, recipient=recipient)
 
 
 @receiver(order_fully_created)
 def send_email_after_order(sender, order_items, user, total_sum, **kwargs):
     subject = f"Спасибо за заказ, {user.username}!"
-    recipient = user.email
+    recipient = [user.email]
     lines = []
     for item in order_items:
         lines.append(f"{item.product.name} — {item.quantity} шт. по {item.price} ₽")
